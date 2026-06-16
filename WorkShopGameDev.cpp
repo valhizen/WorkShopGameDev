de0fc6eb483fa0c3403466c5c89d0417e0e01af3
Valhizen <valhizen@gmail.com>
2026-06-16 15:13:01 +0545

    Add project files.

---
+++ WorkShopGameDev.cpp
@@ -0,0 +1,12 @@
+﻿// WorkShopGameDev.cpp : Defines the entry point for the application.
+//
+
+#include "WorkShopGameDev.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
